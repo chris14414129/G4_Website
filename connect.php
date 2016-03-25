568891a68e95475cd6d85f92f2550b757c6f9ddf
14403286 <clem.clem@hotmail.co.uk>
2016-03-25 22:19:16 +0000

    Delete connect.php

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-//used for connecting to the SQL server and setting up a variable for use in functions
-$server = '194.81.104.22';
-$username = 'group4_1516';
-$password = 'group4';
-//The name of the schema we created earlier in MySQL workbench
-$schema = 'db_group4_1516';
-$pdo = new PDO('mysql:dbname=' . $schema . ';host=' . $server, $username, $password);
-?>
